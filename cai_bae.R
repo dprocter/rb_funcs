69aaf1d1848dfe18041edc3d7c15ab8f0f01dfa6
Duncan Procter <dprocter@gmail.com>
2020-03-27 15:27:25 +0000

    Create cai_bae.R

---
+++ cai_bae.R
@@ -0,0 +1,25 @@
+## Set you libpaths as required
+
+library(broom)
+library(openxlsx)
+# Here we use 'tidy' instead of 'lm' as the output is easier to manipulate
+# change 'lm1' to be the name of your model
+trimmed = tidy(lm1)[,-c(3:5)]
+data.frame(trimmed)
+# create an empty list for loop outputted df to go into
+output <- list()
+# 'selected_vars1' must be a list of your predictors
+# here we make 1 table per predictor by looping a filer(where) function
+for (i in selected_vars1) {
+  tmp <- filter(trimmed, grepl(i, term, fixed = TRUE))
+  output[[i]] <- tmp
+}
+# create a blank workbook
+wb <- createWorkbook()
+# create 1 sheet per variable, each sheet is named after a variable.
+lapply(seq_along(output), function(i){
+  addWorksheet(wb=wb, sheetName = names(output[i]))
+  writeData(wb, sheet = i, output[i][-length(output[[i]])])
+})
+#Save Workbook
+saveWorkbook(wb, "test.xlsx", overwrite = TRUE)
